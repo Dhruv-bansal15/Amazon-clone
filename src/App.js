07c7963abb537b9b8075a7e9dc81a7aa30e20561
Dhruv Bansal <bansaldhruv1973@gmail.com>
2022-02-17 15:14:53 +0530

    route setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-// import logo from './logo.svg';
+import React from "react"
 import './App.css';
+import {BrowserRouter as Router,Switch,Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hye Amazon</h1>
-    </div>
+    <Router> 
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>login page</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
