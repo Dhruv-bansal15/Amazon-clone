e1c02027a3798c866c23b36be27ce3ec35b53128
Dhruv Bansal <bansaldhruv1973@gmail.com>
2022-02-19 12:22:23 +0530

    banner complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,64 @@
-import React from 'react'
+import React from "react";
 import "./Home.css";
+import Product from "./Product";
 function Home() {
   return (
     <div className="home">
       <img
-      className='home__image'
+        className="home__image"
         src="https://m.media-amazon.com/images/I/516TM2qDJXL._SX1500_.jpg"
         alt="Amazon"
       />
+      <div className="home__row">
+        <Product
+          id="12356489"
+          title="Boat Rockerz 255 Pro"
+          price={1399}
+          rating={5}
+          image="https://images-eu.ssl-images-amazon.com/images/I/31PU4kWou+L._AC_SX184_.jpg"
+        />
+        <Product
+          id="12356489"
+          title="Boat Rockerz 255 Pro"
+          price={1399}
+          rating={5}
+          image="https://images-eu.ssl-images-amazon.com/images/I/31PU4kWou+L._AC_SX184_.jpg"
+        />
+        <Product
+          id="12356489"
+          title="Boat Rockerz 255 Pro"
+          price={1399}
+          rating={5}
+          image="https://images-eu.ssl-images-amazon.com/images/I/31PU4kWou+L._AC_SX184_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="12356489"
+          title="Boat Rockerz 255 Pro"
+          price={1399}
+          rating={5}
+          image="https://images-eu.ssl-images-amazon.com/images/I/31PU4kWou+L._AC_SX184_.jpg"
+        />
+        <Product
+          id="12356489"
+          title="Boat Rockerz 255 Pro"
+          price={1399}
+          rating={5}
+          image="https://images-eu.ssl-images-amazon.com/images/I/31PU4kWou+L._AC_SX184_.jpg"
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id="12356489"
+          title="Boat Rockerz 255 Pro"
+          price={1399}
+          rating={5}
+          image="https://images-eu.ssl-images-amazon.com/images/I/31PU4kWou+L._AC_SX184_.jpg"
+        />
+      </div>
     </div>
   );
 }
 
-export default Home
+export default Home;
